a2f97161e0c0fb705e985ccf66d48ad0d24ca6f5
Shivane Rana <shivanerana2004@gmail.com>
2024-09-09 21:58:08 +0530

    nothing spectacular

---
+++ algorithm/sorting/quickSort.java
@@ -0,0 +1,29 @@
+class quickSort {
+    public static void main(String[] args) {
+
+        int[] arr = {5,2,1,4,3,2,32,565,7,678,8576,8,768,567,536,75674};
+        System.out.println("Before Sort~");
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+
+
+
+        System.out.println();
+        System.out.println("After Sort~");
+        for(int i: arr){
+            System.out.print(i+" ");
+        }
+    }
+
+    static void quick(int[] arr, int start, int end){
+        int length = arr.length;
+        if(length <= 1) break;
+
+
+    }
+
+    static int partition(int[] arr){
+
+    }
+}
